e3619a5071b951b0656569e10857219306594eb1
mmitchell1727 <mmitchell1727@gmail.com>
2023-06-07 13:23:23 -0400

    Initial commit

---
+++ config.js
@@ -0,0 +1,22 @@
+const defaultAuthConfig = {
+	cacheLocation: 'localstorage',
+};
+
+const config = {
+	auth: {
+		...defaultAuthConfig,
+		domain: 'auth-rocks-scary-tiger.cic-demo-platform.auth0app.com',
+		clientId: 'n83XnMipj2uE9b5s08uOrenprdZsmvif',
+		// UNCOMMENT the following line to test the private API
+		// audience: ['api://authrocks/'],
+	},
+	app: {
+		enableSilentAuth: false,
+		port: 3000,
+	},
+	server: {
+		permissions: ['authRocks'],
+	},
+};
+
+export default config;
